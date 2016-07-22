0e80bfdc210f33ec6113e85048b590b5c96a6e35
mcwww <babooshka57100@gmail.com>
2016-07-22 17:46:58 -0400

    Add 2 analyzers: WeakCipherAnalyzer.cs and WeakCipherModeAnalyzer.cs

---
+++ RoslynSecurityGuard/RoslynSecurityGuard.Test/Tests/WeakCipherAnalyzerTest.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.CodeAnalysis.Diagnostics;
+using RoslynSecurityGuard.Analyzers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestHelper;
+using Microsoft.CodeAnalysis;
+
+namespace RoslynSecurityGuard.Test.Tests
+{
+    [TestClass]
+    public class WeakCipherAnalyzerTest : DiagnosticVerifier
+    {
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzers()
+        {
+            return new WeakCipherAnalyzer();
+        }
+
+        [TestMethod]
+        public void WeakCipherFalsePositive()
+        {
+            var test = @"
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+class WeakCipherAlgorithm
+    {
+    static void EncryptTextToFileTripleDES(String Data, String FileName, byte[] Key, byte[] IV)
+        {
+                // Create or open the specified file.
+                FileStream fStream = File.Open(FileName, FileMode.OpenOrCreate);
+
+                // Create a new TripleDES object.
+                TripleDES tripleDESalg = TripleDES.Create();
+
+                // Create a CryptoStream using the FileStream  
+                // and the passed key and initialization vector (IV).
+                CryptoStream cStream = new CryptoStream(fStream,
+                    tripleDESalg.CreateEncryptor(Key, IV),
+                    CryptoStreamMode.Write);
+
+                // Create a StreamWriter using the CryptoStream.
+                StreamWriter sWriter = new StreamWriter(cStream);
+
+                // Write the data to the stream  
+                // to encrypt it.
+                sWriter.WriteLine(Data);
+
+                // Close the streams and 
+                // close the file.
+                sWriter.Close();
+                cStream.Close();
+                fStream.Close();
+        }
+    }";
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void WeakCipherVulnerableDES()
+        {
+            var test = @"
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+class WeakCipherAlgorithm
+{
+
+        private static byte[] EncryptDataDES(String inName, String outName, byte[] desKey, byte[] desIV,String Data)
+        {
+            byte[] zeroIV = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            byte[] key = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 0, 0, 0, 0, 0, 0, 0, 0 };
+            // Create a new DES object to generate a key 
+            // and initialization vector (IV).
+            DES DESalg = DES.Create();
+
+            // Create a string to encrypt. 
+            byte[] encrypted;
+
+            ICryptoTransform encryptor = DESalg.CreateEncryptor(key, zeroIV);
+
+            // Create the streams used for encryption. 
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+
+                        //Write all data to the stream.
+                        swEncrypt.Write(Data);
+                    }
+                    encrypted = msEncrypt.ToArray();
+                    return encrypted;
+                }
+            }
+        }
+}";
+            var expected = new DiagnosticResult
+            {
+                Id = "SG0010",
+                Severity = DiagnosticSeverity.Warning
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+
+        [TestMethod]
+        public void WeakCipherVulnerableDES2()
+        {
+            var test = @"
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+class WeakCipherAlgorithm
+{
+private static void EncryptData(String inName, String outName, byte[] desKey, byte[] desIV)
+        {
+            //Create the file streams to handle the input and output files.
+            FileStream fin = new FileStream(inName, FileMode.Open, FileAccess.Read);
+            FileStream fout = new FileStream(outName, FileMode.OpenOrCreate, FileAccess.Write);
+            fout.SetLength(0);
+
+            //Create variables to help with read and write. 
+            byte[] bin = new byte[100]; //This is intermediate storage for the encryption. 
+            long rdlen = 0;              //This is the total number of bytes written. 
+            long totlen = fin.Length;    //This is the total length of the input file. 
+            int len;                     //This is the number of bytes to be written at a time.
+
+            DES des = new DESCryptoServiceProvider();
+            CryptoStream encStream = new CryptoStream(fout, des.CreateEncryptor(desKey, desIV), CryptoStreamMode.Write);
+
+
+            //Read from the input file, then encrypt and write to the output file. 
+            while (rdlen < totlen)
+            {
+                len = fin.Read(bin, 0, 100);
+                encStream.Write(bin, 0, len);
+                rdlen = rdlen + len;
+            }
+
+            encStream.Close();
+            fout.Close();
+            fin.Close();
+        }
+    }
+}";
+            var expected = new DiagnosticResult
+            {
+                Id = "SG0010",
+                Severity = DiagnosticSeverity.Warning
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+    }
+}
---
+++ RoslynSecurityGuard/RoslynSecurityGuard/Analyzers/WeakCipherModeAnalyzer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+namespace RoslynSecurityGuard.Analyzers
+{
+  public class WeakCipherModeAnalyzer : DiagnosticAnalyzer
+    {
+        private static DiagnosticDescriptor Rule = AnalyzerUtil.GetDescriptorFromResource("SG0012", typeof(WeakCipherModeAnalyzer).Name, DiagnosticSeverity.Warning);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(VisitSyntaxNode, SyntaxKind.InvocationExpression);
+        }
+
+        private static void VisitSyntaxNode(SyntaxNodeAnalysisContext ctx)
+        {
+            MemberAccessExpressionSyntax node = ctx.Node as MemberAccessExpressionSyntax;
+            if (node != null)
+            {
+                var symbol = ctx.SemanticModel.GetSymbolInfo(node).Symbol;
+                //DES.Create()
+                if (AnalyzerUtil.InvokeMatch(symbol, className: "CipherMode", method: "ECB"))
+                {
+                    var diagnostic = Diagnostic.Create(Rule, node.Expression.GetLocation(), new string[] { "ECB" });
+                    ctx.ReportDiagnostic(diagnostic);
+                }
+                if (AnalyzerUtil.InvokeMatch(symbol, className: "CipherMode", method: "OFB"))
+                {
+                    var diagnostic = Diagnostic.Create(Rule, node.Expression.GetLocation(), new string[] { "OFB" });
+                    ctx.ReportDiagnostic(diagnostic);
+                }
+            }
+        }
+    }
+}

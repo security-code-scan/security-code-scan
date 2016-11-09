fab69372cfe0eaa2d8fd86164d9309d095795400
Antonius Mulder <tonymulder@gmail.com>
2016-11-08 23:26:44 -0500

    Fix - Update messages to be more dynamic Fix - Messages to align & support multiple unsupported algorithms Add - Tests for RC2

--- RoslynSecurityGuard/RoslynSecurityGuard.Test/Tests/WeakCipherAnalyzerTest.cs
+++ RoslynSecurityGuard/RoslynSecurityGuard.Test/Tests/WeakCipherAnalyzerTest.cs
@@ -118,6 +118,59 @@ class WeakCipherAlgorithm
             VerifyCSharpDiagnostic(test, expected);
         }
 
+        [TestMethod]
+        public void WeakCipherVulnerableRC2()
+        {
+            var test = @"
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+class WeakCipherAlgorithm
+{
+
+        private static byte[] EncryptDataRC2(String inName, String outName, byte[] desKey, byte[] desIV,String Data)
+        {
+            byte[] zeroIV = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            byte[] key = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 0, 0, 0, 0, 0, 0, 0, 0 };
+            // Create a new RC2 object to generate a key 
+            // and initialization vector (IV).
+            RC2 RC2alg = RC2.Create();
+
+            // Create a string to encrypt. 
+            byte[] encrypted;
+
+            ICryptoTransform encryptor = RC2alg.CreateEncryptor(key, zeroIV);
+
+            // Create the streams used for encryption. 
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+
+                        //Write all data to the stream.
+                        swEncrypt.Write(Data);
+                    }
+                    encrypted = msEncrypt.ToArray();
+                    return encrypted;
+                }
+            }
+        }
+}";
+            var expected = new DiagnosticResult
+            {
+                Id = "SG0010",
+                Severity = DiagnosticSeverity.Warning
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
 
         [TestMethod]
         public void WeakCipherVulnerableDES2()
@@ -173,5 +226,58 @@ private static void EncryptData(String inName, String outName, byte[] desKey, by
             VerifyCSharpDiagnostic(test, expected);
         }
 
+        [TestMethod]
+        public void WeakCipherVulnerableRC2_2()
+        {
+            var test = @"
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+class WeakCipherAlgorithm
+{
+private static void EncryptData(String inName, String outName, byte[] rc2Key, byte[] rc2IV)
+        {
+            //Create the file streams to handle the input and output files.
+            FileStream fin = new FileStream(inName, FileMode.Open, FileAccess.Read);
+            FileStream fout = new FileStream(outName, FileMode.OpenOrCreate, FileAccess.Write);
+            fout.SetLength(0);
+
+            //Create variables to help with read and write. 
+            byte[] bin = new byte[100]; //This is intermediate storage for the encryption. 
+            long rdlen = 0;              //This is the total number of bytes written. 
+            long totlen = fin.Length;    //This is the total length of the input file. 
+            int len;                     //This is the number of bytes to be written at a time.
+
+            RC2 rc2 = new RC2CryptoServiceProvider();
+            CryptoStream encStream = new CryptoStream(fout, des.CreateEncryptor(rc2Key, rc2IV), CryptoStreamMode.Write);
+
+
+            //Read from the input file, then encrypt and write to the output file. 
+            while (rdlen < totlen)
+            {
+                len = fin.Read(bin, 0, 100);
+                encStream.Write(bin, 0, len);
+                rdlen = rdlen + len;
+            }
+
+            encStream.Close();
+            fout.Close();
+            fin.Close();
+        }
+    }
+}";
+            var expected = new DiagnosticResult
+            {
+                Id = "SG0010",
+                Severity = DiagnosticSeverity.Warning
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
     }
 }

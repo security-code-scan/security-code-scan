afcdf9579856a5c3ed16fe363635a252dac2d0db
Philippe Arteau <parteau@gosecure.ca>
2016-11-10 11:53:59 -0500

    CSRF Token detection #25

---
+++ RoslynSecurityGuard/RoslynSecurityGuard.Test/Tests/CsrfTokenAnalyzerTest.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoslynSecurityGuard.Analyzers;
+using RoslynSecurityGuard.Analyzers.Taint;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using TestHelper;
+
+namespace RoslynSecurityGuard.Test.Tests
+{
+    [TestClass]
+    public class CsrfTokenAnalyzerTest : DiagnosticVerifier
+    {
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzers()
+        {
+            return new CsrfTokenAnalyzer();
+        }
+        
+        protected override IEnumerable<MetadataReference> GetAdditionnalReferences()
+        {
+            return new[] { MetadataReference.CreateFromFile(typeof(ValidateAntiForgeryTokenAttribute).Assembly.Location) };
+        }
+        
+        [TestMethod]
+        public void DetectMissingToken()
+        {
+            var test = @"
+using System;
+using System.Diagnostics;
+
+namespace VulnerableApp
+{
+    public class TestController
+    {
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult ControllerMethod(string input) {
+
+            return null;
+        }
+    }
+}
+";
+            var expected = new DiagnosticResult
+            {
+                Id = "SG0016",
+                Severity = DiagnosticSeverity.Warning
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+
+        [TestMethod]
+        public void ValidateAntiForgeryTokenPresent()
+        {
+            var test = @"
+using System;
+using System.Diagnostics;
+
+namespace VulnerableApp
+{
+    public class TestController
+    {
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ControllerMethod(string input) {
+
+            return null;
+        }
+    }
+}
+";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ControllerMethod(string input) {
+
+            return null;
+        }
+    }
+}

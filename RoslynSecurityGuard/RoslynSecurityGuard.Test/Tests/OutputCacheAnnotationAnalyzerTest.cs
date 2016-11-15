7d8fe1232ba70063c92569f3876000dfce523a48
Philippe Arteau <parteau@gosecure.ca>
2016-11-14 22:00:03 -0500

    Detect cached authorized pages #32

---
+++ RoslynSecurityGuard/RoslynSecurityGuard.Test/Tests/OutputCacheAnnotationAnalyzerTest.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoslynSecurityGuard.Analyzers;
+
+using System.Collections.Generic;
+
+using System.Web.Mvc;
+using TestHelper;
+
+namespace RoslynSecurityGuard.Test.Tests
+{
+    [TestClass]
+    public class OutputCacheAnnotationAnalyzerTest : DiagnosticVerifier
+    {
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzers()
+        {
+            return new OutputCacheAnnotationAnalyzer();
+        }
+        
+        protected override IEnumerable<MetadataReference> GetAdditionnalReferences()
+        {
+            return new[] { MetadataReference.CreateFromFile(typeof(OutputCacheAttribute).Assembly.Location) };
+        }
+
+        [TestMethod]
+        public void DetectAnnotation1()
+        {
+            var test = @"
+using System;
+using System.Web.Mvc;
+
+[Authorize]
+public class HomeController : Controller
+{
+    [OutputCache]        
+    public ActionResult Index()
+    {
+        return View();
+    }
+}
+";
+            var expected = new DiagnosticResult
+            {
+                Id = "SG0019",
+                Severity = DiagnosticSeverity.Warning
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        [TestMethod]
+        public void DetectAnnotation2()
+        {
+            var test = @"
+using System;
+using System.Web.Mvc;
+
+public class HomeController : Controller
+{
+    [Authorize]
+    [OutputCache]        
+    public ActionResult Index()
+    {
+        return View();
+    }
+}
+";
+            var expected = new DiagnosticResult
+            {
+                Id = "SG0019",
+                Severity = DiagnosticSeverity.Warning
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        [TestMethod]
+        public void FalsePositive1()
+        {
+            var test = @"
+using System;
+using System.Web.Mvc;
+
+public class HomeController : Controller
+{
+    [OutputCache]        
+    public ActionResult Index()
+    {
+        return View();
+    }
+}
+";
+            var expected = new DiagnosticResult
+            {
+                Id = "SG0019",
+                Severity = DiagnosticSeverity.Warning
+            };
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+
+        [Authorize]
+        [OutputCache]
+        public ActionResult ControllerMethod()
+        {
+
+            return null;
+        }
+    }
+}
---
+++ RoslynSecurityGuard/RoslynSecurityGuard/Analyzers/OutputCacheAnnotationAnalyzer.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using RoslynSecurityGuard.Analyzers.Locale;
+using RoslynSecurityGuard.Analyzers.Utils;
+using System.Collections.Immutable;
+
+namespace RoslynSecurityGuard.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class OutputCacheAnnotationAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "SG0019";
+        private static DiagnosticDescriptor Rule = LocaleUtil.GetDescriptor(DiagnosticId);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(VisitClass, SyntaxKind.ClassDeclaration);
+        }
+
+        private void VisitClass(SyntaxNodeAnalysisContext ctx)
+        {
+            var node = ctx.Node as ClassDeclarationSyntax;
+
+            if (node == null)
+            { //Not the expected node type
+                return;
+            }
+
+            var classHasAuthAnnotation = false;
+            var classHasCacheAnnotation = false;
+
+            AnalyzerUtil.ForEachAnnotation(node.AttributeLists, 
+                delegate (string Name, AttributeSyntax att) {
+                    if (Name == "Authorize") {
+                        classHasAuthAnnotation = true;
+                    }
+                    else if (Name == "OutputCache")
+                    {
+                        classHasCacheAnnotation = true;
+                    }
+                }
+            );
+
+            foreach (MemberDeclarationSyntax member in node.Members) {
+                var method = member as MethodDeclarationSyntax;
+                if (method == null) continue;
+
+                var methodHasAuthAnnotation = false;
+                var methodHasCacheAnnotation = false;
+                AnalyzerUtil.ForEachAnnotation(method.AttributeLists,
+                    delegate (string Name, AttributeSyntax att) {
+                        if (Name == "Authorize")
+                        {
+                            methodHasAuthAnnotation = true;
+                        }
+                        else if (Name == "OutputCache")
+                        {
+                            methodHasCacheAnnotation = true;
+                        }
+                    }
+                );
+
+                bool hasAuth = classHasAuthAnnotation || methodHasAuthAnnotation;
+                bool hasCache = classHasCacheAnnotation || methodHasCacheAnnotation;
+
+                if (hasAuth && hasCache) {
+                    ctx.ReportDiagnostic(Diagnostic.Create(Rule, method.GetLocation()));
+                }
+            }
+
+        }
+    }
+}

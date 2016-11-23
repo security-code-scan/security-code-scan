a32b9f4a6dc23e657c3f58517c6b11dff9a0c122
Philippe Arteau <parteau@gosecure.ca>
2016-11-23 02:08:53 -0500

    Update website generator

--- website/generate_static_website.py
+++ website/generate_static_website.py
@@ -1,6 +1,7 @@
 from jinja2 import Environment, FileSystemLoader
 import os
 import yaml
+from collections import OrderedDict
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -16,7 +17,7 @@ def render_template(filename,**args):
         fh.write(footer_html)
 
 #Loading rule descriptions
-rules = {}
+rules = OrderedDict()
 with open("../RoslynSecurityGuard/RoslynSecurityGuard/Messages.yml", 'r') as stream:
     try:
         data = yaml.load(stream)
@@ -35,7 +36,7 @@ with open("../RoslynSecurityGuard/RoslynSecurityGuard/Sinks.yml", 'r') as stream
     try:
         data = yaml.load(stream)
         nb_sinks = len(data)
-        print nb_sinks
+        print "%s injection sinks" % (nb_sinks)
     except yaml.YAMLError as exc:
         print(exc)
 
@@ -45,14 +46,14 @@ with open("../RoslynSecurityGuard/RoslynSecurityGuard/Passwords.yml", 'r') as st
     try:
         data = yaml.load(stream)
         nb_passwords = len(data)
-        print nb_sinks
+        print "%s passwords sinks" % (nb_passwords)
     except yaml.YAMLError as exc:
         print(exc)
 
 #Building the complete website
 
-download_link = "https://visualstudiogallery.msdn.microsoft.com/28033821-c053-4652-b529-0bad9c0feba4/referral/232680"
-version = '2.0.0'
+download_link = "https://marketplace.visualstudio.com/items?itemName=PhilippeArteau.RoslynSecurityGuard"
+version = '2.1.0'
 
 render_template('index.htm', title='Home' , latest_version=version, nb_rules=len(rules), nb_signatures=(len(rules)+nb_sinks+nb_passwords), download_link = download_link)
 render_template('rules.htm', title='Rules', rules=rules)

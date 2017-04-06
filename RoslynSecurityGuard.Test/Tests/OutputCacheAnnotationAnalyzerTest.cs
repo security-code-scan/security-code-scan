8cc40a96e880294d3548291543527f8c744dc0f4
jl <jarlob@gmail.com>
2017-04-07 00:53:23 +0300

    OutputCache false positives fixes

--- RoslynSecurityGuard.Test/Tests/OutputCacheAnnotationAnalyzerTest.cs
+++ RoslynSecurityGuard.Test/Tests/OutputCacheAnnotationAnalyzerTest.cs
@@ -2,9 +2,7 @@
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RoslynSecurityGuard.Analyzers;
-
 using System.Collections.Generic;
-
 using System.Web.Mvc;
 using TestHelper;
 
@@ -17,7 +15,7 @@ namespace RoslynSecurityGuard.Test.Tests
         {
             return new[] { new OutputCacheAnnotationAnalyzer() };
         }
-        
+
         protected override IEnumerable<MetadataReference> GetAdditionnalReferences()
         {
             return new[] { MetadataReference.CreateFromFile(typeof(OutputCacheAttribute).Assembly.Location) };
@@ -33,7 +31,7 @@ using System.Web.Mvc;
 [Authorize]
 public class HomeController : Controller
 {
-    [OutputCache]        
+    [OutputCache]
     public ActionResult Index()
     {
         return View();
@@ -42,7 +40,7 @@ public class HomeController : Controller
 ";
             var expected = new DiagnosticResult
             {
-                Id = "SG0019",
+                Id = OutputCacheAnnotationAnalyzer.DiagnosticId,
                 Severity = DiagnosticSeverity.Warning
             };
 
@@ -59,7 +57,7 @@ using System.Web.Mvc;
 public class HomeController : Controller
 {
     [Authorize]
-    [OutputCache]        
+    [OutputCache]
     public ActionResult Index()
     {
         return View();
@@ -68,12 +66,260 @@ public class HomeController : Controller
 ";
             var expected = new DiagnosticResult
             {
-                Id = "SG0019",
+                Id = OutputCacheAnnotationAnalyzer.DiagnosticId,
                 Severity = DiagnosticSeverity.Warning
             };
 
             VerifyCSharpDiagnostic(test, expected);
         }
+
+        [TestMethod]
+        public void DetectAnnotation3()
+        {
+            var test = @"
+using System;
+using System.Web.Mvc;
+
+[Authorize]
+[OutputCache]
+public class HomeController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+}
+";
+            var expected = new DiagnosticResult
+            {
+                Id = OutputCacheAnnotationAnalyzer.DiagnosticId,
+                Severity = DiagnosticSeverity.Warning
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        [TestMethod]
+        public void DetectAnnotation4()
+        {
+            var test = @"
+using System;
+using System.Web.Mvc;
+
+[Authorize]
+public class MyController : Controller
+{
+}
+
+[OutputCache]
+public class HomeController : MyController
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+}
+";
+            var expected = new DiagnosticResult
+            {
+                Id = OutputCacheAnnotationAnalyzer.DiagnosticId,
+                Severity = DiagnosticSeverity.Warning
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        [TestMethod]
+        public void DetectAnnotation5()
+        {
+            var test = @"
+using System;
+using System.Web.Mvc;
+
+public class MyController : Controller
+{
+    [Authorize]
+    public virtual ActionResult Index()
+    {
+        return null;
+    }
+}
+
+[OutputCache]
+public class HomeController : MyController
+{
+    public override ActionResult Index()
+    {
+        return View();
+    }
+}
+";
+            var expected = new DiagnosticResult
+            {
+                Id = OutputCacheAnnotationAnalyzer.DiagnosticId,
+                Severity = DiagnosticSeverity.Warning
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        [TestMethod]
+        public void DetectAnnotation6()
+        {
+            var test = @"
+using System;
+using System.Web.Mvc;
+
+public abstract class MyController : Controller
+{
+    [Authorize]
+    public abstract ActionResult Index();
+}
+
+[OutputCache]
+public class HomeController : MyController
+{
+    public override ActionResult Index()
+    {
+        return View();
+    }
+}
+";
+            var expected = new DiagnosticResult
+            {
+                Id = OutputCacheAnnotationAnalyzer.DiagnosticId,
+                Severity = DiagnosticSeverity.Warning
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        [TestMethod]
+        public void DetectAnnotation7()
+        {
+            var test = @"
+using System;
+using System.Web.Mvc;
+
+public class MyController : Controller
+{
+    [Authorize]
+    public virtual ActionResult Index()
+    {
+        return null;
+    }
+}
+
+public class HomeController : MyController
+{
+    [OutputCache]
+    public override ActionResult Index()
+    {
+        return View();
+    }
+}
+";
+            var expected = new DiagnosticResult
+            {
+                Id = OutputCacheAnnotationAnalyzer.DiagnosticId,
+                Severity = DiagnosticSeverity.Warning
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        [TestMethod]
+        public void DetectAnnotation8()
+        {
+            var test = @"
+using System;
+using System.Web.Mvc;
+
+[OutputCache(NoStore = true, Duration = 0, VaryByParam = ""*"")]
+public class MyController : Controller
+{
+}
+
+[OutputCache(NoStore = true, Duration = int.MaxValue, VaryByParam = ""*"")]
+public class HomeController : MyController
+{
+    [Authorize]
+    public ActionResult Index()
+    {
+        return View();
+    }
+}
+";
+            var expected = new DiagnosticResult
+            {
+                Id = OutputCacheAnnotationAnalyzer.DiagnosticId,
+                Severity = DiagnosticSeverity.Warning
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+// The question is if we want to go so far and detect a derived attribute since the caching logic can be altered
+
+//        [TestMethod]
+//        public void DetectAnnotation9()
+//        {
+//            var test = @"
+//using System;
+//using System.Web.Mvc;
+
+//class MyAuthorizeAttribute : AuthorizeAttribute
+//{
+//}
+
+//[MyAuthorizeAttribute]
+//[OutputCache]
+//public class HomeController : Controller
+//{
+//    public ActionResult Index()
+//    {
+//        return View();
+//    }
+//}
+//";
+//            var expected = new DiagnosticResult
+//            {
+//                Id = OutputCacheAnnotationAnalyzer.DiagnosticId,
+//                Severity = DiagnosticSeverity.Warning
+//            };
+
+//            VerifyCSharpDiagnostic(test, expected);
+//        }
+//
+//        [TestMethod]
+//        public void DetectAnnotation10()
+//        {
+//            var test = @"
+//using System;
+//using System.Web.Mvc;
+
+//class MyOutputCacheAttribute : OutputCacheAttribute
+//{
+//}
+
+//[AuthorizeAttribute]
+//[MyOutputCache]
+//public class HomeController : Controller
+//{
+//    public ActionResult Index()
+//    {
+//        return View();
+//    }
+//}
+//";
+//            var expected = new DiagnosticResult
+//            {
+//                Id = OutputCacheAnnotationAnalyzer.DiagnosticId,
+//                Severity = DiagnosticSeverity.Warning
+//            };
+
+//            VerifyCSharpDiagnostic(test, expected);
+//        }
 
         [TestMethod]
         public void FalsePositive1()
@@ -84,29 +330,138 @@ using System.Web.Mvc;
 
 public class HomeController : Controller
 {
-    [OutputCache]        
+    [OutputCache]
     public ActionResult Index()
     {
         return View();
     }
 }
 ";
-            var expected = new DiagnosticResult
-            {
-                Id = "SG0019",
-                Severity = DiagnosticSeverity.Warning
-            };
 
             VerifyCSharpDiagnostic(test);
         }
 
-
-        [Authorize]
-        [OutputCache]
-        public ActionResult ControllerMethod()
+        [TestMethod]
+        public void FalsePositive2()
         {
+            var test = @"
+using System;
+using System.Web.Mvc;
 
-            return null;
+[OutputCache(NoStore = true, Duration = 0, VaryByParam = ""*"")]
+public class HomeController : Controller
+{
+    [Authorize]
+    public ActionResult Index()
+    {
+        return View();
+    }
+}
+";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void FalsePositive3()
+        {
+            var test = @"
+using System;
+using System.Web.Mvc;
+
+[OutputCache(NoStore = true, Duration = int.MaxValue, VaryByParam = ""*"")]
+public class MyController : Controller
+{
+}
+
+[OutputCache(NoStore = true, Duration = 0, VaryByParam = ""*"")]
+public class HomeController : MyController
+{
+    [Authorize]
+    public ActionResult Index()
+    {
+        return View();
+    }
+}
+";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void FalsePositive4()
+        {
+            var test = @"
+using System;
+using System.Web.Mvc;
+
+[OutputCache(NoStore = true, Duration = 3600, VaryByParam = ""*"")]
+public class MyController : Controller
+{
+}
+
+public class HomeController : MyController
+{
+    [Authorize]
+    [OutputCache(NoStore = true, Duration = 0, VaryByParam = ""*"")]
+    public ActionResult Index()
+    {
+        return View();
+    }
+}
+";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void FalsePositive5()
+        {
+            var test = @"
+using System;
+using System.Web.Mvc;
+
+[Authorize]
+[OutputCache]
+public class HomeController : Controller
+{
+    protected ActionResult Index()
+    {
+        return View();
+    }
+}
+";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void FalsePositive6()
+        {
+            var test = @"
+using System;
+using System.Web.Mvc;
+
+class AuthorizeAttribute : Attribute
+{
+}
+
+class OutputCacheAttribute : Attribute
+{
+}
+
+[Authorize]
+[OutputCache]
+public class HomeController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+}
+";
+
+            VerifyCSharpDiagnostic(test);
         }
     }
 }

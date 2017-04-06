48d3911e407c8ac5569c6d2099c6775374a6a90b
Francis Prairie <fprairie@gosecure.ca>
2017-04-06 15:50:17 -0400

    Implementation of the PasswordValidator's limit on the RequiredLength

---
+++ RoslynSecurityGuard.Test/Tests/WeakPasswordValidatorAnalyzerTest.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestHelper;
+using Microsoft.CodeAnalysis.Diagnostics;
+using RoslynSecurityGuard.Analyzers;
+using Microsoft.CodeAnalysis;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+namespace RoslynSecurityGuard.Test.Tests
+{
+	[TestClass]
+	public class WeakPasswordValidatorAnalyzerTest : DiagnosticVerifier
+	{
+		protected override IEnumerable<DiagnosticAnalyzer> GetCSharpDiagnosticAnalyzers()
+		{
+			return new[] { new WeakPasswordValidatorAnalyzer() };
+		}
+
+		protected override IEnumerable<MetadataReference> GetAdditionnalReferences()
+		{
+			return new[] { MetadataReference.CreateFromFile(typeof(Controller).Assembly.Location),
+				MetadataReference.CreateFromFile(typeof(System.Linq.Enumerable).Assembly.Location),
+				MetadataReference.CreateFromFile(typeof(PasswordValidator).Assembly.Location) };
+		}
+
+		[TestMethod]
+		public void PasswordValidatorDeclarationTooShort()
+		{
+			var test = @"
+				using Microsoft.AspNet.Identity;
+				using System.Web.Mvc;
+
+				namespace WebApplicationSandbox.Controllers
+				{
+					public class HomeController : Controller
+					{
+						public ActionResult Index()
+						{
+							PasswordValidator pwdv = new PasswordValidator
+							{
+								RequiredLength = 7,
+								RequireNonLetterOrDigit = true,
+								RequireDigit = true,
+								RequireLowercase = true,
+								RequireUppercase = true,
+							};
+
+							return View();
+						}
+					}
+				}";
+
+			var expected = new DiagnosticResult
+			{
+				Id = "SG0032",
+				Severity = DiagnosticSeverity.Warning
+			};
+
+			VerifyCSharpDiagnostic(test, expected);
+		}
+
+		[TestMethod]
+		public void PasswordValidatorTooShort()
+		{
+			var test = @"
+				using Microsoft.AspNet.Identity;
+				using System.Web.Mvc;
+
+				namespace WebApplicationSandbox.Controllers
+				{
+					public class HomeController : Controller
+					{
+						public ActionResult Index()
+						{
+							PasswordValidator pwdv = new PasswordValidator
+							{
+				
+							};
+
+							pwdv.RequiredLength = 6;
+
+							return View();
+						}
+					}
+				}";
+
+			var expected = new DiagnosticResult
+			{
+				Id = "SG0032",
+				Severity = DiagnosticSeverity.Warning
+			};
+
+			VerifyCSharpDiagnostic(test, expected);
+		}
+	}
+}
+
+
+
---
+++ RoslynSecurityGuard/Analyzers/WeakPasswordValidatorAnalyzer.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using RoslynSecurityGuard.Analyzers.Locale;
+using RoslynSecurityGuard.Analyzers.Taint;
+using RoslynSecurityGuard.Analyzers.Utils;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoslynSecurityGuard.Analyzers
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public class WeakPasswordValidatorAnalyzer : DiagnosticAnalyzer
+	{
+		private static DiagnosticDescriptor RulePasswordLength = LocaleUtil.GetDescriptor("SG0032");
+		private static DiagnosticDescriptor RulePasswordValidators = LocaleUtil.GetDescriptor("SG0033");
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(RulePasswordLength, RulePasswordValidators);
+
+		public override void Initialize(AnalysisContext context)
+		{
+			context.RegisterSyntaxNodeAction(VisitAssignment, SyntaxKind.SimpleAssignmentExpression, SyntaxKind.VariableDeclaration);
+		}
+		
+		private static void VisitAssignment(SyntaxNodeAnalysisContext ctx)
+		{
+			AssignmentExpressionSyntax node = ctx.Node as AssignmentExpressionSyntax;
+
+			var symbol = ctx.SemanticModel.GetSymbolInfo(node.Left).Symbol;
+
+			var content = node.Right.GetText().ToString();
+
+			if (AnalyzerUtil.SymbolMatch(symbol, name: "RequiredLength") && content != String.Empty && Convert.ToInt32(content) < 8)
+			{
+				int numericValue;
+				if (!(int.TryParse(node.Right.GetText().ToString(), out numericValue) && numericValue > 8))
+				{
+					var diagnostic = Diagnostic.Create(RulePasswordLength, node.GetLocation());
+					ctx.ReportDiagnostic(diagnostic);
+				}
+			}
+		}
+	// //
+	}
+}

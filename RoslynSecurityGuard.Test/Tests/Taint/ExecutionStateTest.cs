1b54f578ac4a87e01593c08676fec4be06b8d334
jl <jarlob@gmail.com>
2017-04-07 00:38:05 +0300

    GetValueByIdentifier was returning zeroed structure that caused InsecureCookieAnalyzer: System.NullReferenceException

---
+++ RoslynSecurityGuard.Test/Tests/Taint/ExecutionStateTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoslynSecurityGuard.Analyzers.Taint;
+
+namespace RoslynSecurityGuard.Tests
+{
+    [TestClass]
+    public class ExecutionStateTest
+    {
+        [TestMethod]
+        public void GetValueByIdentifierTest()
+        {
+            var ctx = new SyntaxNodeAnalysisContext();
+            var executionState = new ExecutionState(ctx);
+
+            var variableState = executionState.GetValueByIdentifier("none");
+            Assert.IsNotNull(variableState.tags);
+            Assert.AreEqual(VariableTaint.UNKNOWN, variableState.taint);
+        }
+    }
+}

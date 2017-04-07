ae52579d67349ff4223d5b32ef7df3d74a106764
jl <jarlob@gmail.com>
2017-04-07 15:00:45 +0300

    Path traversal fix. Sinks fixed for some overloads.

--- RoslynSecurityGuard.Test/Tests/Taint/PathTraversalAnalyzerTest.cs
+++ RoslynSecurityGuard.Test/Tests/Taint/PathTraversalAnalyzerTest.cs
@@ -4,6 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RoslynSecurityGuard.Analyzers.Taint;
 using System.Collections.Generic;
 using System.IO;
+using System.Xml;
 using TestHelper;
 
 namespace RoslynSecurityGuard.Test.Tests.Taint
@@ -19,7 +20,7 @@ namespace RoslynSecurityGuard.Test.Tests.Taint
 
         protected override IEnumerable<MetadataReference> GetAdditionnalReferences()
         {
-            return new[] { MetadataReference.CreateFromFile(typeof(File).Assembly.Location) };
+            return new[] { MetadataReference.CreateFromFile(typeof(File).Assembly.Location), MetadataReference.CreateFromFile(typeof(XmlReader).Assembly.Location) };
         }
 
 
@@ -113,6 +114,51 @@ class PathTraversal
         }
 
         [TestMethod]
+        public void PathTraversalFound5()
+        {
+            var test = @"
+using System.IO;
+
+class PathTraversal
+{
+    public static void Run(string input)
+    {
+        new StreamReader(input, System.Text.Encoding.ASCII, false, 0);
+    }
+}
+";
+            var expected = new DiagnosticResult
+            {
+                Id = "SG0018",
+                Severity = DiagnosticSeverity.Warning,
+            };
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        [TestMethod]
+        public void PathTraversalFound6()
+        {
+            var test = @"
+using System.Xml;
+
+class PathTraversal
+{
+    public static void Run(string input)
+    {
+        XmlReaderSettings settings = new XmlReaderSettings();
+        XmlReader reader = XmlReader.Create(input, settings, (XmlParserContext)null);
+    }
+}
+";
+            var expected = new DiagnosticResult
+            {
+                Id = "SG0018",
+                Severity = DiagnosticSeverity.Warning,
+            };
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        [TestMethod]
         public void FalsePositive1()
         {
             var test = @"
@@ -123,23 +169,6 @@ class PathTraversal
     public static void Run(string input)
     {
         return File.OpenRead(""C:/static/fsociety.dat"");
-    }
-}
-";
-            VerifyCSharpDiagnostic(test);
-        }
-
-        [TestMethod]
-        public void FalsePositive2()
-        {
-            var test = @"
-using System.IO;
-
-class PathTraversal
-{
-    public static void Run(string input)
-    {
-        new StreamReader(input, false, System.Text.Encoding.ASCII, 0);
     }
 }
 ";

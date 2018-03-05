4b84273d1c8c6a1db66141d73b7408742bb86686
Viktorija <viktorija.adomauskaite@gmail.com>
2018-03-05 17:53:52 +0200

    Added Request.Unvalidated method and property validation (#3)
    Added Request.Unvalidated method and property validation

--- SecurityCodeScan.Test/Tests/RequestValidationAnalyzerTest.cs
+++ SecurityCodeScan.Test/Tests/RequestValidationAnalyzerTest.cs
@@ -20,7 +20,11 @@ namespace SecurityCodeScan.Test
 
         private static readonly PortableExecutableReference[] References =
         {
-            MetadataReference.CreateFromFile(typeof(ValidateInputAttribute).Assembly.Location)
+            MetadataReference.CreateFromFile(typeof(ValidateInputAttribute).Assembly.Location),
+            MetadataReference.CreateFromFile(typeof(System.Web.HttpRequestBase).Assembly.Location),
+#pragma warning disable 618
+            MetadataReference.CreateFromFile(typeof(System.Web.Helpers.Validation).Assembly.Location)
+#pragma warning restore 618
         };
 
         protected override IEnumerable<MetadataReference> GetAdditionalReferences() => References;
@@ -66,6 +70,189 @@ End Namespace
 
             await VerifyCSharpDiagnostic(cSharpTest, expected).ConfigureAwait(false);
             await VerifyVisualBasicDiagnostic(visualBasicTest, expected).ConfigureAwait(false);
+        }
+
+        [TestMethod]
+        public async Task DetectUnvalidatedProperty()
+        {
+            var cSharpTest = @"
+using System.Web.Mvc;
+
+namespace VulnerableApp
+{
+    public class TestController : Controller
+    {
+        [HttpPost]
+        public ActionResult ControllerMethod(string input) {
+            var test = Request.Unvalidated;
+            return null;
+        }
+    }
+}
+";
+
+            var visualBasicTest = @"
+Imports System.Web.Mvc
+
+Namespace VulnerableApp
+    Public Class TestController
+        Inherits Controller
+        <HttpPost> _
+        Public Function ControllerMethod(input As String) As ActionResult
+            Dim test = Request.Unvalidated
+            Return Nothing
+        End Function
+    End Class
+End Namespace
+";
+
+            var expected = new DiagnosticResult
+            {
+                Id = "SCS0017",
+                Severity = DiagnosticSeverity.Warning
+            };
+
+            await VerifyCSharpDiagnostic(cSharpTest, expected.WithLocation("Test0.cs", 10, 32)).ConfigureAwait(false);
+            await VerifyVisualBasicDiagnostic(visualBasicTest, expected.WithLocation("Test0.vb", 9, 32)).ConfigureAwait(false);
+        }
+
+        [TestMethod]
+        public async Task DetectUnvalidatedMethod()
+        {
+            var cSharpTest = @"
+using System.Web.Mvc;
+using System.Web.Helpers;
+
+namespace VulnerableApp
+{
+    public class TestController : Controller
+    {
+        [HttpPost]
+        public ActionResult ControllerMethod(string input) {
+            var test = Request.Unvalidated();
+            return null;
+        }
+    }
+}
+";
+
+            var visualBasicTest = @"
+Imports System.Web.Mvc
+Imports System.Web.Helpers
+
+Namespace VulnerableApp
+    Public Class TestController
+        Inherits Controller
+        <HttpPost> _
+        Public Function ControllerMethod(input As String) As ActionResult
+#Disable Warning BC40000
+            Dim test = Validation.Unvalidated(Request)
+#Enable Warning BC40000
+            Return Nothing
+        End Function
+    End Class
+End Namespace
+";
+
+            var expected = new DiagnosticResult
+            {
+                Id = "SCS0017",
+                Severity = DiagnosticSeverity.Warning
+            };
+
+            await VerifyCSharpDiagnostic(cSharpTest, expected.WithLocation("Test0.cs", 11, 32)).ConfigureAwait(false);
+            await VerifyVisualBasicDiagnostic(visualBasicTest, expected.WithLocation("Test.vb", 11, 35)).ConfigureAwait(false); 
+        }
+
+        [TestMethod]
+        public async Task IgnoreUnrelatedUnvalidatedMethod()
+        {
+
+            var cSharpTest = @"
+using System.Web.Mvc;
+
+namespace VulnerableApp
+{
+    public class TestController
+    {
+        public void Unvalidated(){
+        }
+        [HttpPost]
+        public ActionResult ControllerMethod(string input) {
+            Unvalidated();
+            return null;
+        }
+    }
+}
+";
+
+            var visualBasicTest = @"
+Imports System.Web.Mvc
+
+Namespace VulnerableApp
+    Public Class TestController
+        Public Sub Unvalidated()
+        End Sub
+        <HttpPost> _
+        Public Function ControllerMethod(input As String) As ActionResult
+            Unvalidated()
+            Return Nothing
+        End Function
+    End Class
+End Namespace
+";
+
+
+            await VerifyCSharpDiagnostic(cSharpTest).ConfigureAwait(false);
+            await VerifyVisualBasicDiagnostic(visualBasicTest).ConfigureAwait(false);
+        }
+
+        [TestMethod]
+        public async Task IgnoreUnrelatedStaticUnvalidatedMethod()
+        {
+
+            var cSharpTest = @"
+using System.Web.Mvc;
+
+namespace VulnerableApp
+{
+    public class TestController : Controller
+    {
+        public class Test
+        {
+            public static void Unvalidated(){
+            }
+        }
+        [HttpPost]
+        public ActionResult ControllerMethod(string input) {
+            Test.Unvalidated();
+            return null;
+        }
+    }
+}
+";
+
+            var visualBasicTest = @"
+Imports System.Web.Mvc
+
+Namespace VulnerableApp
+    Public Class TestController
+        Class Test
+            Public Shared Sub Unvalidated()
+            End Sub
+        End Class
+        <HttpPost> _
+        Public Function ControllerMethod(input As String) As ActionResult
+            Test.Unvalidated()
+            Return Nothing
+        End Function
+    End Class
+End Namespace
+";
+
+
+            await VerifyCSharpDiagnostic(cSharpTest).ConfigureAwait(false);
+            await VerifyVisualBasicDiagnostic(visualBasicTest).ConfigureAwait(false);
         }
     }
 }

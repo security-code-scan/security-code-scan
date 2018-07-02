d15fd823c05392478c921df851761573b4d737d8
Viktorija <viktorija.adomauskaite@gmail.com>
2018-07-02 18:54:40 +0300

    moved csrf token types to config (#60)
    * moved csrf token types to config
    
    * rename test file
    
    * requested fixes
    
    * added comment in Config\main.yml

--- SecurityCodeScan/Config/Configuration.cs
+++ SecurityCodeScan/Config/Configuration.cs
@@ -7,17 +7,20 @@ namespace SecurityCodeScan.Config
     {
         public Configuration()
         {
-            Behavior = new Dictionary<string, KeyValuePair<string, MethodBehavior>>();
-            Sinks    = new Dictionary<string, KeyValuePair<string, MethodBehavior>>();
+            Behavior           = new Dictionary<string, KeyValuePair<string, MethodBehavior>>();
+            Sinks              = new Dictionary<string, KeyValuePair<string, MethodBehavior>>();
+            AntiCsrfAttributes = new Dictionary<string, List<string>>();
         }
 
         public Configuration(Configuration config)
         {
-            Behavior = new Dictionary<string, KeyValuePair<string, MethodBehavior>>(config.Behavior);
-            Sinks    = new Dictionary<string, KeyValuePair<string, MethodBehavior>>(config.Sinks);
+            Behavior           = new Dictionary<string, KeyValuePair<string, MethodBehavior>>(config.Behavior);
+            Sinks              = new Dictionary<string, KeyValuePair<string, MethodBehavior>>(config.Sinks);
+            AntiCsrfAttributes = new Dictionary<string, List<string>>(config.AntiCsrfAttributes);
         }
 
         public Dictionary<string, KeyValuePair<string, MethodBehavior>> Behavior;
         public Dictionary<string, KeyValuePair<string, MethodBehavior>> Sinks;
+        public Dictionary<string, List<string>>                         AntiCsrfAttributes;
     }
 }

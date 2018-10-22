a9e48d6010f18fc731a1dacb0b78dcb45a10702c
zaichenko <26891270+zaichenko@users.noreply.github.com>
2018-10-22 16:26:13 +0300

    Added search for sql injection in methods of Entity Framework and Microsoft Enterprise Library

--- SecurityCodeScan.Test/Tests/Taint/SqlInjectionAnalyzerTest.cs
+++ SecurityCodeScan.Test/Tests/Taint/SqlInjectionAnalyzerTest.cs
@@ -18,24 +18,30 @@ namespace SecurityCodeScan.Test.Taint
 
         private static readonly PortableExecutableReference[] References =
         {            
-            MetadataReference.CreateFromFile(typeof(System.Web.UI.WebControls.SqlDataSource).Assembly.Location)
-        };
-
-        //private static readonly PortableExecutableReference[] References =
-        //{
-        //    MetadataReference.CreateFromFile(typeof(Microsoft.AspNetCore.Mvc.ValidateAntiForgeryTokenAttribute).Assembly.Location),
-        //    MetadataReference.CreateFromFile(typeof(Microsoft.AspNetCore.Mvc.HttpPostAttribute).Assembly.Location),
-        //    MetadataReference.CreateFromFile(typeof(System.Web.Mvc.ValidateAntiForgeryTokenAttribute).Assembly.Location)
-        //};
+            MetadataReference.CreateFromFile(typeof(System.Web.UI.WebControls.SqlDataSource).Assembly.Location),
+            MetadataReference.CreateFromFile(typeof(System.Data.Entity.DbContext).Assembly.Location),
+            MetadataReference.CreateFromFile(typeof(Microsoft.Practices.EnterpriseLibrary.Data.Sql.SqlDatabase).Assembly.Location)
+        };       
 
         protected override IEnumerable<MetadataReference> GetAdditionalReferences() => References;
 
 
-        [DataRow("var sda = new SqlDataAdapter(sqlQuery, new SqlConnection());")]
-        [DataRow("var sds = new SqlDataSource(\"connectionString\", sqlQuery);")]
-        [DataRow("var sds = new SqlDataSource(\"providerName\",\"connectionString\", sqlQuery);")]
+        [DataRow("var temp = new SqlDataAdapter(sqlQuery, new SqlConnection())")]
+        [DataRow("var temp = new SqlDataSource(\"connectionString\", sqlQuery)")]
+        [DataRow("var temp = new SqlDataSource(\"providerName\",\"connectionString\", sqlQuery)")]
+        [DataRow("var temp = new DbContext(\"connectionString\").Database.SqlQuery(null, sqlQuery, null)")]
+        [DataRow("var temp = new DbContext(\"connectionString\").Database.ExecuteSqlCommand(sqlQuery, parameters)")]
+        [DataRow("var temp = new DbContext(\"connectionString\").Database.ExecuteSqlCommand(TransactionalBehavior.DoNotEnsureTransaction, sqlQuery, parameters)")]
+        [DataRow("var temp = new DbContext(\"connectionString\").Database.ExecuteSqlCommandAsync(sqlQuery, parameters)")]
+        [DataRow("var temp = new DbContext(\"connectionString\").Database.ExecuteSqlCommandAsync(TransactionalBehavior.DoNotEnsureTransaction, sqlQuery, parameters)")]
+        [DataRow("var temp = new DbContext(\"connectionString\").Database.ExecuteSqlCommandAsync(sqlQuery, new CancellationToken(), parameters)")]
+        [DataRow("var temp = new DbContext(\"connectionString\").Database.ExecuteSqlCommandAsync(TransactionalBehavior.DoNotEnsureTransaction, sqlQuery, new CancellationToken(), parameters)")]
+        [DataRow("var temp = new SqlDatabase(\"connectionString\").ExecuteDataSet(CommandType.Text, sqlQuery)")]
+        [DataRow("var temp = new SqlDatabase(\"connectionString\").ExecuteReader(CommandType.Text, sqlQuery)")]
+        [DataRow("var temp = new SqlDatabase(\"connectionString\").ExecuteNonQuery(CommandType.Text, sqlQuery)")]
+        [DataRow("var temp = new SqlDatabase(\"connectionString\").ExecuteScalar(CommandType.Text, sqlQuery)")]
         [DataTestMethod]
-        public async Task SqlInjectionVulnerableCSharp(string sink)
+        public async Task SqlInjectionVulnerable(string sink)
         {
             var cSharpTest = $@"
 #pragma warning disable 8019
@@ -43,47 +49,41 @@ namespace SecurityCodeScan.Test.Taint
     using System.Data.Common;
     using System.Data;
     using System.Web.UI.WebControls;
+    using System.Data.Entity;
+    using System.Threading;
+    using Microsoft.Practices.EnterpriseLibrary.Data.Sql;
 #pragma warning restore 8019
 
 namespace sample
 {{
     class MyFoo
     {{
-        public static void Run(string sqlQuery)
+        public static void Run(string sqlQuery, params object[] parameters)
         {{
-            {sink}
+            {sink};
         }}       
     }}
 }}
 ";
-            var expected = new DiagnosticResult
-            {
-                Id = "SCS0014",
-                Severity = DiagnosticSeverity.Warning,
-            };
 
-            await VerifyCSharpDiagnostic(cSharpTest, expected).ConfigureAwait(false);            
-        }
-
-
-
-        [DataRow("Dim sda = New SqlDataAdapter(sqlQuery, New SqlConnection())")]
-        [DataRow("Dim sds = New SqlDataSource(\"connectionString\", sqlQuery)")]
-        [DataRow("Dim sds = New SqlDataSource(\"providerName\",\"connectionString\", sqlQuery)")]
-        [DataTestMethod]
-        public async Task SqlInjectionVulnerableVBasic(string sink)
-        {
+            sink = sink.Replace("null", "Nothing")
+                .Replace("var ", "Dim ")
+                .Replace("new ", "New ");
+            
             var visualBasicTest = $@"
 #Disable Warning BC50001
     Imports System.Data.SqlClient
     Imports System.Data.Common
     Imports System.Data
     Imports System.Web.UI.WebControls
+    Imports System.Data.Entity
+    Imports System.Threading
+    Imports Microsoft.Practices.EnterpriseLibrary.Data.Sql
 #Enable Warning BC50001
 
 Namespace sample
     Class MyFoo
-        Public Shared Sub Run(sqlQuery As System.String)
+        Public Shared Sub Run(sqlQuery As System.String, ParamArray parameters() As Object)
             {sink}
         End Sub
     End Class
@@ -95,7 +95,9 @@ End Namespace
                 Severity = DiagnosticSeverity.Warning,
             };
 
-            await VerifyVisualBasicDiagnostic(visualBasicTest, expected).ConfigureAwait(false);           
+            await VerifyCSharpDiagnostic(cSharpTest, expected).ConfigureAwait(false);
+            await VerifyVisualBasicDiagnostic(visualBasicTest, expected).ConfigureAwait(false);
         }
+
     }
 }

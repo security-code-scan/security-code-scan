eb975c3caf54441ea6d522274678e328b0de28a8
zaichenko <26891270+zaichenko@users.noreply.github.com>
2018-10-30 20:19:56 +0300

    Added search for XPath injection in several methods

--- SecurityCodeScan.Test/Tests/Taint/XPathInjectionAnalyzerTest.cs
+++ SecurityCodeScan.Test/Tests/Taint/XPathInjectionAnalyzerTest.cs
@@ -17,6 +17,83 @@ namespace SecurityCodeScan.Test.Taint
             return new DiagnosticAnalyzer[] { new TaintAnalyzerCSharp(), new TaintAnalyzerVisualBasic(), };
         }
 
+        [DataRow("nav.SelectSingleNode(input)", true)]
+        [DataRow("nav.SelectSingleNode(\"input\")", false)]
+        [DataRow("nav.SelectSingleNode(input, null)", true)]
+        [DataRow("nav.SelectSingleNode(\"input\", null)", false)]
+        [DataRow("nav.SelectSingleNode(nav.Compile(input))", true)]
+        [DataRow("nav.SelectSingleNode(nav.Compile(\"input\"))", false)]
+        [DataRow("nav.Select(input)", true)]
+        [DataRow("nav.Select(\"input\")", false)]
+        [DataRow("nav.Select(input, null)", true)]
+        [DataRow("nav.Select(\"input\", null)", false)]
+        [DataRow("nav.Select(nav.Compile(input))", true)]
+        [DataRow("nav.Select(nav.Compile(\"input\"))", false)]
+        [DataRow("nav.Compile(input)", true)]
+        [DataRow("nav.Compile(\"input\")", false)]
+        [DataRow("nav.Evaluate(input)", true)]
+        [DataRow("nav.Evaluate(\"input\")", false)]
+        [DataRow("nav.Evaluate(input, null)", true)]
+        [DataRow("nav.Evaluate(\"input\", null)", false)]
+        [DataRow("nav.Evaluate(nav.Compile(input))", true)]
+        [DataRow("nav.Evaluate(nav.Compile(\"input\"))", false)]
+        [DataRow("nav.Evaluate(nav.Compile(input), null)", true)]
+        [DataRow("nav.Evaluate(nav.Compile(\"input\"), null)", false)]
+        [DataTestMethod]
+        public async Task XPathInjection(string sink, bool warn)
+        {
+            var cSharpTest = $@"
+#pragma warning disable 8019
+    using System.Xml;
+    using System.Xml.XPath;
+#pragma warning restore 8019
+
+namespace sample
+{{
+    class MyFoo
+    {{
+        public static void Run(XPathNavigator nav, string input)
+        {{
+            {sink};
+        }}
+    }}
+}}
+";
+
+            sink = sink.Replace("null", "Nothing").Replace("new ", "New ");
+
+            var visualBasicTest = $@"
+#Disable Warning BC50001    
+    Imports System.Xml
+    Imports System.Xml.XPath
+#Enable Warning BC50001
+
+Namespace sample
+    Class MyFoo
+        Public Shared Sub Run(nav As XPathNavigator, input As System.String)
+            {sink}
+        End Sub
+    End Class
+End Namespace
+";
+            var expected = new DiagnosticResult
+            {
+                Id = "SCS0003",
+                Severity = DiagnosticSeverity.Warning,
+            };
+
+            if (warn)
+            {
+                await VerifyCSharpDiagnostic(cSharpTest, expected).ConfigureAwait(false);
+                await VerifyVisualBasicDiagnostic(visualBasicTest, expected).ConfigureAwait(false);
+            }
+            else
+            {
+                await VerifyCSharpDiagnostic(cSharpTest).ConfigureAwait(false);
+                await VerifyVisualBasicDiagnostic(visualBasicTest).ConfigureAwait(false);
+            }
+        }
+
         //No diagnostics expected to show up
         [TestMethod]
         public async Task XPathInjectionFalsePositive()

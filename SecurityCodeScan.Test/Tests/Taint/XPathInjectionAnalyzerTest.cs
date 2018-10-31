85080bd2d33669c55ad0862feb124e607b8ff45c
zaichenko <26891270+zaichenko@users.noreply.github.com>
2018-10-31 17:44:40 +0300

    Changed XPathInjectionAnalyzerTest by analogy with SqlInjectionAnalyzerTest.

--- SecurityCodeScan.Test/Tests/Taint/XPathInjectionAnalyzerTest.cs
+++ SecurityCodeScan.Test/Tests/Taint/XPathInjectionAnalyzerTest.cs
@@ -17,28 +17,36 @@ namespace SecurityCodeScan.Test.Taint
             return new DiagnosticAnalyzer[] { new TaintAnalyzerCSharp(), new TaintAnalyzerVisualBasic(), };
         }
 
-        [DataRow("nav.SelectSingleNode(input)", true)]
-        [DataRow("nav.SelectSingleNode(\"input\")", false)]
-        [DataRow("nav.SelectSingleNode(input, null)", true)]
-        [DataRow("nav.SelectSingleNode(\"input\", null)", false)]
-        [DataRow("nav.SelectSingleNode(nav.Compile(input))", true)]
+        [DataRow("doc.SelectNodes(input)",                       true)]
+        [DataRow("doc.SelectNodes(\"input\")",                   false)]
+        [DataRow("doc.SelectNodes(input, null)",                 true)]
+        [DataRow("doc.SelectNodes(\"input\", null)",             false)]
+        [DataRow("doc.SelectSingleNode(input)",                  true)]
+        [DataRow("doc.SelectSingleNode(\"input\")",              false)]
+        [DataRow("doc.SelectSingleNode(input, null)",            true)]
+        [DataRow("doc.SelectSingleNode(\"input\", null)",        false)]
+        [DataRow("nav.SelectSingleNode(input)",                  true)]
+        [DataRow("nav.SelectSingleNode(\"input\")",              false)]
+        [DataRow("nav.SelectSingleNode(input, null)",            true)]
+        [DataRow("nav.SelectSingleNode(\"input\", null)",        false)]
+        [DataRow("nav.SelectSingleNode(nav.Compile(input))",     true)]
         [DataRow("nav.SelectSingleNode(nav.Compile(\"input\"))", false)]
-        [DataRow("nav.Select(input)", true)]
-        [DataRow("nav.Select(\"input\")", false)]
-        [DataRow("nav.Select(input, null)", true)]
-        [DataRow("nav.Select(\"input\", null)", false)]
-        [DataRow("nav.Select(nav.Compile(input))", true)]
-        [DataRow("nav.Select(nav.Compile(\"input\"))", false)]
-        [DataRow("nav.Compile(input)", true)]
-        [DataRow("nav.Compile(\"input\")", false)]
-        [DataRow("nav.Evaluate(input)", true)]
-        [DataRow("nav.Evaluate(\"input\")", false)]
-        [DataRow("nav.Evaluate(input, null)", true)]
-        [DataRow("nav.Evaluate(\"input\", null)", false)]
-        [DataRow("nav.Evaluate(nav.Compile(input))", true)]
-        [DataRow("nav.Evaluate(nav.Compile(\"input\"))", false)]
-        [DataRow("nav.Evaluate(nav.Compile(input), null)", true)]
-        [DataRow("nav.Evaluate(nav.Compile(\"input\"), null)", false)]
+        [DataRow("nav.Select(input)",                            true)]
+        [DataRow("nav.Select(\"input\")",                        false)]
+        [DataRow("nav.Select(input, null)",                      true)]
+        [DataRow("nav.Select(\"input\", null)",                  false)]
+        [DataRow("nav.Select(nav.Compile(input))",               true)]
+        [DataRow("nav.Select(nav.Compile(\"input\"))",           false)]
+        [DataRow("nav.Compile(input)",                           true)]
+        [DataRow("nav.Compile(\"input\")",                       false)]
+        [DataRow("nav.Evaluate(input)",                          true)]
+        [DataRow("nav.Evaluate(\"input\")",                      false)]
+        [DataRow("nav.Evaluate(input, null)",                    true)]
+        [DataRow("nav.Evaluate(\"input\", null)",                false)]
+        [DataRow("nav.Evaluate(nav.Compile(input))",             true)]
+        [DataRow("nav.Evaluate(nav.Compile(\"input\"))",         false)]
+        [DataRow("nav.Evaluate(nav.Compile(input), null)",       true)]
+        [DataRow("nav.Evaluate(nav.Compile(\"input\"), null)",   false)]
         [DataTestMethod]
         public async Task XPathInjection(string sink, bool warn)
         {
@@ -52,7 +60,7 @@ namespace sample
 {{
     class MyFoo
     {{
-        public static void Run(XPathNavigator nav, string input)
+        public static void Run(XmlDocument doc, XPathNavigator nav, string input)
         {{
             {sink};
         }}
@@ -70,7 +78,7 @@ namespace sample
 
 Namespace sample
     Class MyFoo
-        Public Shared Sub Run(nav As XPathNavigator, input As System.String)
+        Public Shared Sub Run(doc As XmlDocument, nav As XPathNavigator, input As System.String)
             {sink}
         End Sub
     End Class
@@ -92,92 +100,6 @@ End Namespace
                 await VerifyCSharpDiagnostic(cSharpTest).ConfigureAwait(false);
                 await VerifyVisualBasicDiagnostic(visualBasicTest).ConfigureAwait(false);
             }
-        }
-
-        //No diagnostics expected to show up
-        [TestMethod]
-        public async Task XPathInjectionFalsePositive()
-        {
-            var cSharpTest = @"
-using System.Xml;
-
-class XPathInjectionTP
-{
-
-    public void vulnerableCases(string input) {
-        XmlDocument doc = new XmlDocument();
-        doc.Load(""/secret_config.xml"");
-
-        doc.SelectNodes(""/Config/Devices/Device[id='1337']"");
-        doc.SelectSingleNode(""/Config/Devices/Device[type='2600']"");
-    }
-}";
-
-            var visualBasicTest = @"
-Imports System.Xml
-
-Class XPathInjectionTP
-    Public Sub vulnerableCases(input As String)
-        Dim doc As New XmlDocument()
-        doc.Load("" / secret_config.xml"")
-        doc.SelectNodes(""/Config/Devices/Device[id='1337']"")
-        doc.SelectSingleNode(""/Config/Devices/Device[type='2600']"")
-    End Sub
-End Class
-";
-
-            await VerifyCSharpDiagnostic(cSharpTest).ConfigureAwait(false);
-            await VerifyVisualBasicDiagnostic(visualBasicTest).ConfigureAwait(false);
-        }
-
-        [TestMethod]
-        public async Task XPathInjectionVulnerable1()
-        {
-            var cSharpTest = @"
-using System.Xml;
-
-class XPathInjectionTP
-{
-
-    public void vulnerableCases(string input) {
-        XmlDocument doc = new XmlDocument();
-        doc.Load(""/secret_config.xml"");
-
-        doc.SelectNodes(""/Config/Devices/Device[id='"" + input + ""']"");
-        doc.SelectSingleNode(""/Config/Devices/Device[type='"" + input + ""']"");
-    }
-}";
-
-            var visualBasicTest = @"
-Imports System.Xml
-
-Class XPathInjectionTP
-    Public Sub vulnerableCases(input As String)
-        Dim doc As New XmlDocument()
-        doc.Load("" / secret_config.xml"")
-        doc.SelectNodes(""/Config/Devices/Device[id='"" & input & ""']"")
-        doc.SelectSingleNode(""/Config/Devices/Device[type='"" & input & ""']"")
-    End Sub
-End Class
-";
-
-            //Two occurrences
-            var expected = new[]
-            {
-                new DiagnosticResult
-                {
-                    Id       = "SCS0003",
-                    Severity = DiagnosticSeverity.Warning
-                },
-                new DiagnosticResult
-                {
-                    Id       = "SCS0003",
-                    Severity = DiagnosticSeverity.Warning
-                }
-            };
-
-            await VerifyCSharpDiagnostic(cSharpTest, expected).ConfigureAwait(false);
-            await VerifyVisualBasicDiagnostic(visualBasicTest, expected).ConfigureAwait(false);
-        }
+        }        
     }
 }

12fc7fda468617c64e6d12b7ea53175ccd552a4f
zaichenko <26891270+zaichenko@users.noreply.github.com>
2018-11-02 19:03:13 +0300

    Added search for XPath injection in XPathExpression.Compile, XPathSelectElement, XPathSelectElements and XPathEvaluate. Renamed \"input\" to \"constant\" Removed trailing whitespaces.

--- SecurityCodeScan.Test/Tests/Taint/XPathInjectionAnalyzerTest.cs
+++ SecurityCodeScan.Test/Tests/Taint/XPathInjectionAnalyzerTest.cs
@@ -17,36 +17,59 @@ namespace SecurityCodeScan.Test.Taint
             return new DiagnosticAnalyzer[] { new TaintAnalyzerCSharp(), new TaintAnalyzerVisualBasic(), };
         }
 
-        [DataRow("doc.SelectNodes(input)",                       true)]
-        [DataRow("doc.SelectNodes(\"input\")",                   false)]
-        [DataRow("doc.SelectNodes(input, null)",                 true)]
-        [DataRow("doc.SelectNodes(\"input\", null)",             false)]
-        [DataRow("doc.SelectSingleNode(input)",                  true)]
-        [DataRow("doc.SelectSingleNode(\"input\")",              false)]
-        [DataRow("doc.SelectSingleNode(input, null)",            true)]
-        [DataRow("doc.SelectSingleNode(\"input\", null)",        false)]
-        [DataRow("nav.SelectSingleNode(input)",                  true)]
-        [DataRow("nav.SelectSingleNode(\"input\")",              false)]
-        [DataRow("nav.SelectSingleNode(input, null)",            true)]
-        [DataRow("nav.SelectSingleNode(\"input\", null)",        false)]
-        [DataRow("nav.SelectSingleNode(nav.Compile(input))",     true)]
-        [DataRow("nav.SelectSingleNode(nav.Compile(\"input\"))", false)]
-        [DataRow("nav.Select(input)",                            true)]
-        [DataRow("nav.Select(\"input\")",                        false)]
-        [DataRow("nav.Select(input, null)",                      true)]
-        [DataRow("nav.Select(\"input\", null)",                  false)]
-        [DataRow("nav.Select(nav.Compile(input))",               true)]
-        [DataRow("nav.Select(nav.Compile(\"input\"))",           false)]
-        [DataRow("nav.Compile(input)",                           true)]
-        [DataRow("nav.Compile(\"input\")",                       false)]
-        [DataRow("nav.Evaluate(input)",                          true)]
-        [DataRow("nav.Evaluate(\"input\")",                      false)]
-        [DataRow("nav.Evaluate(input, null)",                    true)]
-        [DataRow("nav.Evaluate(\"input\", null)",                false)]
-        [DataRow("nav.Evaluate(nav.Compile(input))",             true)]
-        [DataRow("nav.Evaluate(nav.Compile(\"input\"))",         false)]
-        [DataRow("nav.Evaluate(nav.Compile(input), null)",       true)]
-        [DataRow("nav.Evaluate(nav.Compile(\"input\"), null)",   false)]
+        private static readonly PortableExecutableReference[] References =
+        {
+            MetadataReference.CreateFromFile(typeof(System.Xml.Linq.XElement).Assembly.Location)
+        };
+
+        protected override IEnumerable<MetadataReference> GetAdditionalReferences() => References;
+
+        [DataRow("doc.SelectNodes(input)",                           true)]
+        [DataRow("doc.SelectNodes(\"constant\")",                   false)]
+        [DataRow("doc.SelectNodes(input, null)",                     true)]
+        [DataRow("doc.SelectNodes(\"constant\", null)",             false)]
+        [DataRow("doc.SelectSingleNode(input)",                      true)]
+        [DataRow("doc.SelectSingleNode(\"constant\")",              false)]
+        [DataRow("doc.SelectSingleNode(input, null)",                true)]
+        [DataRow("doc.SelectSingleNode(\"constant\", null)",        false)]
+        [DataRow("nav.SelectSingleNode(input)",                      true)]
+        [DataRow("nav.SelectSingleNode(\"constant\")",              false)]
+        [DataRow("nav.SelectSingleNode(input, null)",                true)]
+        [DataRow("nav.SelectSingleNode(\"constant\", null)",        false)]
+        [DataRow("nav.SelectSingleNode(nav.Compile(input))",         true)]
+        [DataRow("nav.SelectSingleNode(nav.Compile(\"constant\"))", false)]
+        [DataRow("nav.Select(input)",                                true)]
+        [DataRow("nav.Select(\"constant\")",                        false)]
+        [DataRow("nav.Select(input, null)",                          true)]
+        [DataRow("nav.Select(\"constant\", null)",                  false)]
+        [DataRow("nav.Select(nav.Compile(input))",                   true)]
+        [DataRow("nav.Select(nav.Compile(\"constant\"))",           false)]
+        [DataRow("nav.Compile(input)",                               true)]
+        [DataRow("nav.Compile(\"constant\")",                       false)]
+        [DataRow("nav.Evaluate(input)",                              true)]
+        [DataRow("nav.Evaluate(\"constant\")",                      false)]
+        [DataRow("nav.Evaluate(input, null)",                        true)]
+        [DataRow("nav.Evaluate(\"constant\", null)",                false)]
+        [DataRow("nav.Evaluate(nav.Compile(input))",                 true)]
+        [DataRow("nav.Evaluate(nav.Compile(\"constant\"))",         false)]
+        [DataRow("nav.Evaluate(nav.Compile(input), null)",           true)]
+        [DataRow("nav.Evaluate(nav.Compile(\"constant\"), null)",   false)]
+        [DataRow("XPathExpression.Compile(input)",                   true)]
+        [DataRow("XPathExpression.Compile(\"constant\")",           false)]
+        [DataRow("XPathExpression.Compile(input, null)",             true)]
+        [DataRow("XPathExpression.Compile(\"constant\", null)",     false)]
+        [DataRow("element.XPathSelectElement(input)",                true)]
+        [DataRow("element.XPathSelectElement(\"constant\")",        false)]
+        [DataRow("element.XPathSelectElement(input, null)",          true)]
+        [DataRow("element.XPathSelectElement(\"constant\", null)",  false)]
+        [DataRow("element.XPathSelectElements(input)",               true)]
+        [DataRow("element.XPathSelectElements(\"constant\")",       false)]
+        [DataRow("element.XPathSelectElements(input, null)",         true)]
+        [DataRow("element.XPathSelectElements(\"constant\", null)", false)]
+        [DataRow("element.XPathEvaluate(input)",                     true)]
+        [DataRow("element.XPathEvaluate(\"constant\")",             false)]
+        [DataRow("element.XPathEvaluate(input, null)",               true)]
+        [DataRow("element.XPathEvaluate(\"constant\", null)",       false)]
         [DataTestMethod]
         public async Task XPathInjection(string sink, bool warn)
         {
@@ -54,31 +77,31 @@ namespace SecurityCodeScan.Test.Taint
 #pragma warning disable 8019
     using System.Xml;
     using System.Xml.XPath;
+    using System.Xml.Linq;
 #pragma warning restore 8019
 
 namespace sample
 {{
     class MyFoo
     {{
-        public static void Run(XmlDocument doc, XPathNavigator nav, string input)
+        public static void Run(XmlDocument doc, XPathNavigator nav, XElement element, string input)
         {{
             {sink};
         }}
     }}
 }}
 ";
-
             sink = sink.Replace("null", "Nothing").Replace("new ", "New ");
-
             var visualBasicTest = $@"
-#Disable Warning BC50001    
+#Disable Warning BC50001
     Imports System.Xml
     Imports System.Xml.XPath
+    Imports System.Xml.Linq
 #Enable Warning BC50001
 
 Namespace sample
     Class MyFoo
-        Public Shared Sub Run(doc As XmlDocument, nav As XPathNavigator, input As System.String)
+        Public Shared Sub Run(doc As XmlDocument, nav As XPathNavigator, element As XElement, input As System.String)
             {sink}
         End Sub
     End Class
@@ -100,6 +123,6 @@ End Namespace
                 await VerifyCSharpDiagnostic(cSharpTest).ConfigureAwait(false);
                 await VerifyVisualBasicDiagnostic(visualBasicTest).ConfigureAwait(false);
             }
-        }        
+        }
     }
 }

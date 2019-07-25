255fd5a71730e144e26b833c39e6e54adf9f3ea6
kevin-montrose <kevin.montrose@gmail.com>
2019-07-25 12:52:18 -0400

    remove debug code

--- SecurityCodeScan/Config/Configuration.cs
+++ SecurityCodeScan/Config/Configuration.cs
@@ -12,11 +12,6 @@ namespace SecurityCodeScan.Config
     /// </summary>
     internal class Configuration
     {
-        static Configuration()
-        {
-            System.Diagnostics.Debugger.Launch();
-        }
-
         public Configuration()
         {
             _PasswordValidatorRequiredProperties = new HashSet<string>();

ec20322cf314a8f489422df3e82b071782b5561a
Jaroslav Lobačevski <jarlob@gmail.com>
2019-08-02 00:09:34 +0300

    NetworkCredential rule fixed. Closes #130

--- SecurityCodeScan.Test/Tests/Password/HardcodedPasswordTest.cs
+++ SecurityCodeScan.Test/Tests/Password/HardcodedPasswordTest.cs
@@ -23,6 +23,49 @@ namespace SecurityCodeScan.Test.Password
 
         [TestCategory("Detect")]
         [TestMethod]
+        public async Task HardCodePasswordNetworkCredential()
+        {
+            var cSharpTest = @"
+using System;
+using System.Net;
+
+namespace VulnerableApp
+{
+    class HardCodedPassword
+    {
+        static void TestHardcodedValue()
+        {
+            var test = new NetworkCredential(Guid.NewGuid().ToString(), ""pass"");
+        }
+    }
+}
+";
+
+            var visualBasicTest = @"
+Imports System
+Imports System.Net
+
+Namespace VulnerableApp
+    Class HardCodedPassword
+        Private Shared Sub TestHardcodedValue()
+            Dim test = New NetworkCredential(Guid.NewGuid().ToString(), ""pass"")
+        End Sub
+    End Class
+End Namespace
+";
+
+            var expected = new DiagnosticResult
+            {
+                Id       = "SCS0015",
+                Severity = DiagnosticSeverity.Warning
+            };
+
+            await VerifyCSharpDiagnostic(cSharpTest, expected).ConfigureAwait(false);
+            await VerifyVisualBasicDiagnostic(visualBasicTest, expected).ConfigureAwait(false);
+        }
+
+        [TestCategory("Detect")]
+        [TestMethod]
         public async Task HardCodePasswordDerivedBytes()
         {
             var cSharpTest = @"

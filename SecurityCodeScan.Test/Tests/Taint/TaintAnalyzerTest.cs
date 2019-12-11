e5bf2c2b0c49f19077b0c9abcabfe4391d39397e
Jaroslav Lobačevski <jarlob@gmail.com>
2019-12-11 17:44:14 +0200

    StringBuilder behaviors added.

--- SecurityCodeScan.Test/Tests/Taint/TaintAnalyzerTest.cs
+++ SecurityCodeScan.Test/Tests/Taint/TaintAnalyzerTest.cs
@@ -461,15 +461,7 @@ Behavior:
     Method:
       InjectableArguments: [SCS0026: 0]
 
-  StringBuilder_ToString:
-    Namespace: System.Text
-    ClassName: StringBuilder
-    Name: ToString
-    Method:
-      Returns:
-        TaintFromArguments: [This]
-
-  StringBuilder_Apped:
+  StringBuilder_Append:
     Namespace: System.Text
     ClassName: StringBuilder
     Name: Append

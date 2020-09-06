dfcd57d6611085b5110dab148aa4de88315809d2
Jaroslav Lobačevski <jarlob@gmail.com>
2020-09-06 17:07:27 +0300

    XSLT settings analyzer

---
+++ SecurityCodeScan.Test/Tests/XsltSettingsTest.cs
@@ -0,0 +1,497 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecurityCodeScan.Analyzers;
+using SecurityCodeScan.Test.Config;
+using SecurityCodeScan.Test.Helpers;
+using DiagnosticVerifier = SecurityCodeScan.Test.Helpers.DiagnosticVerifier;
+
+namespace SecurityCodeScan.Test.InsecureXslt
+{
+    [TestClass]
+    public class XsltSettingsTest : DiagnosticVerifier
+    {
+        protected override IEnumerable<DiagnosticAnalyzer> GetDiagnosticAnalyzers(string _)
+        {
+            return new[] { new XsltSettingsAnalyzer() };
+        }
+
+        private static readonly PortableExecutableReference[] References =
+        {
+            MetadataReference.CreateFromFile(typeof(System.Xml.Xsl.XsltSettings).Assembly.Location),
+        };
+
+        protected override IEnumerable<MetadataReference> GetAdditionalReferences() => References;
+
+        private readonly DiagnosticResult[] Expected =
+        {
+            new DiagnosticResult
+            {
+                Id = "SCS0011",
+                Severity = DiagnosticSeverity.Warning
+            },
+        };
+
+        [TestCategory("Detect")]
+        [TestMethod]
+        public async Task EnabledInitializer()
+        {
+            var cSharpTest = $@"
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace VulnerableApp
+{{
+    class Test
+    {{
+        static void Foo(string xslPath)
+        {{
+            // Create a reader to read books.xml
+            XmlReader reader = XmlReader.Create(""books.xml"");
+
+            // Create a writer for writing the transformed file.
+            XmlWriter writer = XmlWriter.Create(""books.html"");
+
+            // Create and load the transform with script execution enabled.
+            XslCompiledTransform transform = new XslCompiledTransform();
+            XsltSettings settings = new XsltSettings() {{EnableScript = true}};
+            transform.Load(xslPath, settings, null);
+
+            // Execute the transformation.
+            transform.Transform(reader, writer);
+        }}
+    }}
+}}
+";
+
+            var visualBasicTest = $@"
+Imports System.Xml
+Imports System.Xml.Xsl
+
+Namespace VulnerableApp
+    Class Test
+        Private Shared Sub Foo(xslPath As String)
+            Dim reader As XmlReader = XmlReader.Create(""books.xml"")
+            Dim writer As XmlWriter = XmlWriter.Create(""books.html"")
+            Dim transform As XslCompiledTransform = New XslCompiledTransform()
+            Dim settings As XsltSettings = New XsltSettings() With {{
+                .EnableScript = True
+            }}
+            transform.Load(xslPath, settings, Nothing)
+            transform.Transform(reader, writer)
+        End Sub
+    End Class
+End Namespace
+";
+
+            var testConfig = @"
+TaintEntryPoints:
+  VulnerableApp:
+    Method:
+      Name: Foo
+";
+
+            var optionsWithProjectConfig = ConfigurationTest.CreateAnalyzersOptionsWithConfig(testConfig);
+            await VerifyCSharpDiagnostic(cSharpTest, Expected, optionsWithProjectConfig).ConfigureAwait(false);
+            await VerifyVisualBasicDiagnostic(visualBasicTest, Expected, optionsWithProjectConfig).ConfigureAwait(false);
+
+            testConfig = @"
+AuditMode: true
+
+TaintEntryPoints:
+  VulnerableApp:
+    Method:
+      Name: Foo
+";
+
+            optionsWithProjectConfig = ConfigurationTest.CreateAnalyzersOptionsWithConfig(testConfig);
+            await VerifyCSharpDiagnostic(cSharpTest, Expected, optionsWithProjectConfig).ConfigureAwait(false);
+            await VerifyVisualBasicDiagnostic(visualBasicTest, Expected, optionsWithProjectConfig).ConfigureAwait(false);
+        }
+
+        [TestCategory("Detect")]
+        [TestMethod]
+        public async Task EnabledProperty()
+        {
+            var cSharpTest = $@"
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace VulnerableApp
+{{
+    class Test
+    {{
+        static void Foo(string xslPath)
+        {{
+            // Create a reader to read books.xml
+            XmlReader reader = XmlReader.Create(""books.xml"");
+
+            // Create a writer for writing the transformed file.
+            XmlWriter writer = XmlWriter.Create(""books.html"");
+
+            // Create and load the transform with script execution enabled.
+            XslCompiledTransform transform = new XslCompiledTransform();
+            XsltSettings settings = new XsltSettings();
+            settings.EnableScript = true;
+            transform.Load(xslPath, settings, null);
+
+            // Execute the transformation.
+            transform.Transform(reader, writer);
+        }}
+    }}
+}}
+";
+
+            var visualBasicTest = $@"
+Imports System.Xml
+Imports System.Xml.Xsl
+
+Namespace VulnerableApp
+    Class Test
+        Private Shared Sub Foo(xslPath As String)
+            Dim reader As XmlReader = XmlReader.Create(""books.xml"")
+            Dim writer As XmlWriter = XmlWriter.Create(""books.html"")
+            Dim transform As XslCompiledTransform = New XslCompiledTransform()
+            Dim settings As XsltSettings = New XsltSettings()
+            settings.EnableScript = True
+            transform.Load(xslPath, settings, Nothing)
+            transform.Transform(reader, writer)
+        End Sub
+    End Class
+End Namespace
+";
+
+            var testConfig = @"
+TaintEntryPoints:
+  VulnerableApp:
+    Method:
+      Name: Foo
+";
+
+            var optionsWithProjectConfig = ConfigurationTest.CreateAnalyzersOptionsWithConfig(testConfig);
+            await VerifyCSharpDiagnostic(cSharpTest, Expected, optionsWithProjectConfig).ConfigureAwait(false);
+            await VerifyVisualBasicDiagnostic(visualBasicTest, Expected, optionsWithProjectConfig).ConfigureAwait(false);
+
+            testConfig = @"
+AuditMode: true
+
+TaintEntryPoints:
+  VulnerableApp:
+    Method:
+      Name: Foo
+";
+
+            optionsWithProjectConfig = ConfigurationTest.CreateAnalyzersOptionsWithConfig(testConfig);
+            await VerifyCSharpDiagnostic(cSharpTest, Expected, optionsWithProjectConfig).ConfigureAwait(false);
+            await VerifyVisualBasicDiagnostic(visualBasicTest, Expected, optionsWithProjectConfig).ConfigureAwait(false);
+        }
+
+        [TestCategory("Detect")]
+        [TestMethod]
+        public async Task EnabledPropertyUnknown()
+        {
+            var cSharpTest = $@"
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace VulnerableApp
+{{
+    class Test
+    {{
+        static void Foo(string xslPath, bool flag)
+        {{
+            // Create a reader to read books.xml
+            XmlReader reader = XmlReader.Create(""books.xml"");
+
+            // Create a writer for writing the transformed file.
+            XmlWriter writer = XmlWriter.Create(""books.html"");
+
+            // Create and load the transform with script execution enabled.
+            XslCompiledTransform transform = new XslCompiledTransform();
+            XsltSettings settings = new XsltSettings();
+            settings.EnableScript = flag;
+            transform.Load(xslPath, settings, null);
+
+            // Execute the transformation.
+            transform.Transform(reader, writer);
+        }}
+    }}
+}}
+";
+
+            var visualBasicTest = $@"
+Imports System.Xml
+Imports System.Xml.Xsl
+
+Namespace VulnerableApp
+    Class Test
+        Private Shared Sub Foo(xslPath As String, flag As Boolean)
+            Dim reader As XmlReader = XmlReader.Create(""books.xml"")
+            Dim writer As XmlWriter = XmlWriter.Create(""books.html"")
+            Dim transform As XslCompiledTransform = New XslCompiledTransform()
+            Dim settings As XsltSettings = New XsltSettings()
+            settings.EnableScript = flag
+            transform.Load(xslPath, settings, Nothing)
+            transform.Transform(reader, writer)
+        End Sub
+    End Class
+End Namespace
+";
+
+            var testConfig = @"
+TaintEntryPoints:
+  VulnerableApp:
+    Method:
+      Name: Foo
+";
+
+            var optionsWithProjectConfig = ConfigurationTest.CreateAnalyzersOptionsWithConfig(testConfig);
+            await VerifyCSharpDiagnostic(cSharpTest, null, optionsWithProjectConfig).ConfigureAwait(false);
+            await VerifyVisualBasicDiagnostic(visualBasicTest, null, optionsWithProjectConfig).ConfigureAwait(false);
+
+            testConfig = @"
+AuditMode: true
+
+TaintEntryPoints:
+  VulnerableApp:
+    Method:
+      Name: Foo
+";
+
+            optionsWithProjectConfig = ConfigurationTest.CreateAnalyzersOptionsWithConfig(testConfig);
+            await VerifyCSharpDiagnostic(cSharpTest, Expected, optionsWithProjectConfig).ConfigureAwait(false);
+            await VerifyVisualBasicDiagnostic(visualBasicTest, Expected, optionsWithProjectConfig).ConfigureAwait(false);
+        }
+
+        [TestCategory("Safe")]
+        [TestMethod]
+        public async Task DisabledInitializer()
+        {
+            var cSharpTest = $@"
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace VulnerableApp
+{{
+    class Test
+    {{
+        static void Foo(string xslPath)
+        {{
+            // Create a reader to read books.xml
+            XmlReader reader = XmlReader.Create(""books.xml"");
+
+            // Create a writer for writing the transformed file.
+            XmlWriter writer = XmlWriter.Create(""books.html"");
+
+            // Create and load the transform with script execution enabled.
+            XslCompiledTransform transform = new XslCompiledTransform();
+            XsltSettings settings = new XsltSettings() {{EnableScript = false}};
+            transform.Load(xslPath, settings, null);
+
+            // Execute the transformation.
+            transform.Transform(reader, writer);
+        }}
+    }}
+}}
+";
+
+            var visualBasicTest = $@"
+Imports System.Xml
+Imports System.Xml.Xsl
+
+Namespace VulnerableApp
+    Class Test
+        Private Shared Sub Foo(xslPath As String)
+            Dim reader As XmlReader = XmlReader.Create(""books.xml"")
+            Dim writer As XmlWriter = XmlWriter.Create(""books.html"")
+            Dim transform As XslCompiledTransform = New XslCompiledTransform()
+            Dim settings As XsltSettings = New XsltSettings() With {{
+                .EnableScript = False
+            }}
+            transform.Load(xslPath, settings, Nothing)
+            transform.Transform(reader, writer)
+        End Sub
+    End Class
+End Namespace
+";
+
+            var testConfig = @"
+TaintEntryPoints:
+  VulnerableApp:
+    Method:
+      Name: Foo
+";
+
+            var optionsWithProjectConfig = ConfigurationTest.CreateAnalyzersOptionsWithConfig(testConfig);
+            await VerifyCSharpDiagnostic(cSharpTest, null, optionsWithProjectConfig).ConfigureAwait(false);
+            await VerifyVisualBasicDiagnostic(visualBasicTest, null, optionsWithProjectConfig).ConfigureAwait(false);
+
+            testConfig = @"
+AuditMode: true
+
+TaintEntryPoints:
+  VulnerableApp:
+    Method:
+      Name: Foo
+";
+
+            optionsWithProjectConfig = ConfigurationTest.CreateAnalyzersOptionsWithConfig(testConfig);
+            await VerifyCSharpDiagnostic(cSharpTest, null, optionsWithProjectConfig).ConfigureAwait(false);
+            await VerifyVisualBasicDiagnostic(visualBasicTest, null, optionsWithProjectConfig).ConfigureAwait(false);
+        }
+
+        [TestCategory("Safe")]
+        [TestMethod]
+        public async Task DisabledProperty()
+        {
+            var cSharpTest = $@"
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace VulnerableApp
+{{
+    class Test
+    {{
+        static void Foo(string xslPath)
+        {{
+            // Create a reader to read books.xml
+            XmlReader reader = XmlReader.Create(""books.xml"");
+
+            // Create a writer for writing the transformed file.
+            XmlWriter writer = XmlWriter.Create(""books.html"");
+
+            // Create and load the transform with script execution enabled.
+            XslCompiledTransform transform = new XslCompiledTransform();
+            XsltSettings settings = new XsltSettings();
+            settings.EnableScript = false;
+            transform.Load(xslPath, settings, null);
+
+            // Execute the transformation.
+            transform.Transform(reader, writer);
+        }}
+    }}
+}}
+";
+
+            var visualBasicTest = $@"
+Imports System.Xml
+Imports System.Xml.Xsl
+
+Namespace VulnerableApp
+    Class Test
+        Private Shared Sub Foo(xslPath As String)
+            Dim reader As XmlReader = XmlReader.Create(""books.xml"")
+            Dim writer As XmlWriter = XmlWriter.Create(""books.html"")
+            Dim transform As XslCompiledTransform = New XslCompiledTransform()
+            Dim settings As XsltSettings = New XsltSettings()
+            settings.EnableScript = False
+            transform.Load(xslPath, settings, Nothing)
+            transform.Transform(reader, writer)
+        End Sub
+    End Class
+End Namespace
+";
+
+            var testConfig = @"
+TaintEntryPoints:
+  VulnerableApp:
+    Method:
+      Name: Foo
+";
+
+            var optionsWithProjectConfig = ConfigurationTest.CreateAnalyzersOptionsWithConfig(testConfig);
+            await VerifyCSharpDiagnostic(cSharpTest, null, optionsWithProjectConfig).ConfigureAwait(false);
+            await VerifyVisualBasicDiagnostic(visualBasicTest, null, optionsWithProjectConfig).ConfigureAwait(false);
+
+            testConfig = @"
+AuditMode: true
+
+TaintEntryPoints:
+  VulnerableApp:
+    Method:
+      Name: Foo
+";
+
+            optionsWithProjectConfig = ConfigurationTest.CreateAnalyzersOptionsWithConfig(testConfig);
+            await VerifyCSharpDiagnostic(cSharpTest, null, optionsWithProjectConfig).ConfigureAwait(false);
+            await VerifyVisualBasicDiagnostic(visualBasicTest, null, optionsWithProjectConfig).ConfigureAwait(false);
+        }
+
+        [TestCategory("Safe")]
+        [TestMethod]
+        public async Task Default()
+        {
+            var cSharpTest = $@"
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace VulnerableApp
+{{
+    class Test
+    {{
+        static void Foo(string xslPath)
+        {{
+            // Create a reader to read books.xml
+            XmlReader reader = XmlReader.Create(""books.xml"");
+
+            // Create a writer for writing the transformed file.
+            XmlWriter writer = XmlWriter.Create(""books.html"");
+
+            // Create and load the transform with script execution enabled.
+            XslCompiledTransform transform = new XslCompiledTransform();
+            XsltSettings settings = new XsltSettings();
+            transform.Load(xslPath, settings, null);
+
+            // Execute the transformation.
+            transform.Transform(reader, writer);
+        }}
+    }}
+}}
+";
+
+            var visualBasicTest = $@"
+Imports System.Xml
+Imports System.Xml.Xsl
+
+Namespace VulnerableApp
+    Class Test
+        Private Shared Sub Foo(xslPath As String)
+            Dim reader As XmlReader = XmlReader.Create(""books.xml"")
+            Dim writer As XmlWriter = XmlWriter.Create(""books.html"")
+            Dim transform As XslCompiledTransform = New XslCompiledTransform()
+            Dim settings As XsltSettings = New XsltSettings()
+            transform.Load(xslPath, settings, Nothing)
+            transform.Transform(reader, writer)
+        End Sub
+    End Class
+End Namespace
+";
+
+            var testConfig = @"
+TaintEntryPoints:
+  VulnerableApp:
+    Method:
+      Name: Foo
+";
+
+            var optionsWithProjectConfig = ConfigurationTest.CreateAnalyzersOptionsWithConfig(testConfig);
+            await VerifyCSharpDiagnostic(cSharpTest, null, optionsWithProjectConfig).ConfigureAwait(false);
+            await VerifyVisualBasicDiagnostic(visualBasicTest, null, optionsWithProjectConfig).ConfigureAwait(false);
+
+            testConfig = @"
+AuditMode: true
+
+TaintEntryPoints:
+  VulnerableApp:
+    Method:
+      Name: Foo
+";
+
+            optionsWithProjectConfig = ConfigurationTest.CreateAnalyzersOptionsWithConfig(testConfig);
+            await VerifyCSharpDiagnostic(cSharpTest, null, optionsWithProjectConfig).ConfigureAwait(false);
+            await VerifyVisualBasicDiagnostic(visualBasicTest, null, optionsWithProjectConfig).ConfigureAwait(false);
+        }
+    }
+}
---
+++ SecurityCodeScan/Analyzers/XsltSettingsAnalyzer.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+using SecurityCodeScan.Analyzers.Locale;
+using SecurityCodeScan.Analyzers.Utils;
+using SecurityCodeScan.Config;
+
+namespace SecurityCodeScan.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public class XsltSettingsAnalyzer : DiagnosticAnalyzer
+    {
+        public const            string               DiagnosticId = "SCS0011";
+        private static readonly DiagnosticDescriptor Rule         = LocaleUtil.GetDescriptor(DiagnosticId);
+
+        private const string  XsltSettingsTypeName = "System.Xml.Xsl.XsltSettings";
+        private const string  EnableScriptName = "EnableScript";
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            if (!Debugger.IsAttached) // prefer single thread for debugging in development
+                context.EnableConcurrentExecution();
+
+            if (context.IsAuditMode())
+                context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+            else
+                context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            Analyzer.Initialize(context);
+        }
+
+        private class Analyzer
+        {
+            public static void Initialize(AnalysisContext context)
+            {
+                if (!Debugger.IsAttached) // prefer single thread for debugging in development
+                    context.EnableConcurrentExecution();
+
+                if (context.IsAuditMode())
+                    context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+                else
+                    context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+                context.RegisterCompilationStartAction(
+                    (CompilationStartAnalysisContext compilationContext) =>
+                    {
+                        Compilation compilation = compilationContext.Compilation;
+                        var wellKnownTypeProvider = WellKnownTypeProvider.GetOrCreate(compilation);
+
+                        if (!wellKnownTypeProvider.TryGetOrCreateTypeByMetadataName(XsltSettingsTypeName, out var type))
+                        {
+                            return;
+                        }
+
+                        var configuration = Configuration.GetOrCreate(compilationContext);
+
+                        compilationContext.RegisterOperationBlockStartAction(
+                            operationBlockStartContext =>
+                            {
+                                ISymbol owningSymbol = operationBlockStartContext.OwningSymbol;
+                                AnalyzerOptions options = operationBlockStartContext.Options;
+                                CancellationToken cancellationToken = operationBlockStartContext.CancellationToken;
+                                if (owningSymbol.IsConfiguredToSkipAnalysis(options, Rule, compilation, cancellationToken))
+                                {
+                                    return;
+                                }
+
+                                operationBlockStartContext.RegisterOperationAction(
+                                    ctx =>
+                                    {
+                                        IAssignmentOperation operation = (IAssignmentOperation)ctx.Operation;
+                                        if (!(operation.Target is IPropertyReferenceOperation propertyReferenceOperation))
+                                            return;
+
+                                        if (propertyReferenceOperation.Member.ContainingType != type)
+                                        {
+                                            return;
+                                        }
+
+                                        if (propertyReferenceOperation.Member.Name == EnableScriptName &&
+                                            ((operation.Value.ConstantValue.HasValue &&
+                                             operation.Value.ConstantValue.Value is bool isEnableScript && isEnableScript) ||
+                                            !operation.Value.ConstantValue.HasValue && configuration.AuditMode))
+                                        {
+                                            ctx.ReportDiagnostic(Diagnostic.Create(Rule, operation.Syntax.GetLocation()));
+                                        }
+                                    },
+                                    OperationKind.SimpleAssignment);
+                            });
+                    });
+            }
+        }
+    }
+}
